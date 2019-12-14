4ee2ab45fa7e5d695cde8b371486e6ab0adc9b37
Tam Le <tamsgit@gmail.com>
2019-12-14 18:03:24 -0500

    Setup admin page for counter record

---
+++ server/apps/counter/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Counter
+
+
+@admin.register(Counter)
+class CounterAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "count",
+    )
